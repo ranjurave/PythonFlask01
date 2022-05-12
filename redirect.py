0f8087ec1842ad8d810daa53d62793fa80c657c7
ranjurave <39932642+ranjurave@users.noreply.github.com>
2022-05-12 15:15:32 +1200

    initial commit

---
+++ redirect.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, url_for, redirect
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('login.html')
+
+@app.route('/login', methods = ['POST', 'GET'])
+def login():
+    if request.method == 'POST' and request.form['username'] == 'admin':
+        return redirect(url_for('success'))
+    return redirect(url_for('index'))
+@app.route('/success')
+def success():
+    return 'logged in successfully'
+
+if __name__=='__main__':
+    app.run(debug = True)
